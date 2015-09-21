2ce30b1242d3f1a239aba39dfc0542660af5a44a
Steven Nunez <steven.nunez@gmail.com>
2015-09-20 21:48:55 -0400

    Add Events Frontend

--- openspace_frontend/app/router.js
+++ openspace_frontend/app/router.js
@@ -6,6 +6,11 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('events', function(){
+    // this.route('event', {path: "/:event-slug"});
+    this.route('new');
+    this.route('event', {path: ":event_slug"});
+  });
 });
 
 export default Router;
